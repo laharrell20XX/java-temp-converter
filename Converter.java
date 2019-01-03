ee47a0660ef94c16f21e072cde3bb0b83df6745e
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-01-03 10:26:57 -0600

    added methods toKelvin() and toRankine() which convert user provided temperatures to Kelvin and Rankine; added other conditions to toFahrenheit() and toCelsius() that include ways to convert Kelvin and Rankine temperatures

--- Converter.java
+++ Converter.java
@@ -10,8 +10,13 @@ public class Converter {
     public Double toFahrenheit() {
         if (this.unit.equals("F")) {
             return this.magnitude;
+            // refers to the magnitude of the instance of Converter
         } else if (this.unit.equals("C")) {
             return this.magnitude * 9 / 5 + 32;
+        } else if (this.unit.equals("K")) {
+            return (this.magnitude - 273.15) * 9 / 5 + 32;
+        } else if (this.unit.equals("R")) {
+            return this.magnitude - 459.67;
         } else {
             return null;
         }
@@ -21,6 +26,40 @@ public class Converter {
         if (this.unit.equals("F")) {
             return (this.magnitude - 32) * 5 / 9;
         } else if (this.unit.equals("C")) {
+            return this.magnitude;
+        } else if (this.unit.equals("K")) {
+            return this.magnitude - 273.15;
+        } else if (this.unit.equals("R")) {
+            return (this.magnitude - 491.67) * 5 / 9;
+        } else {
+            return null;
+        }
+    }
+
+    public Double toKelvin() {
+        if (this.unit.equals("F")) {
+            return (this.magnitude - 32) * 5 / 9 + 273.15;
+            // refers to the magnitude of the instance of Converter
+        } else if (this.unit.equals("C")) {
+            return this.magnitude + 273.15;
+        } else if (this.unit.equals("K")) {
+            return this.magnitude;
+        } else if (this.unit.equals("R")) {
+            return this.magnitude * 5 / 9;
+        } else {
+            return null;
+        }
+    }
+
+    public Double toRankine() {
+        if (this.unit.equals("F")) {
+            return this.magnitude + 459.67;
+            // refers to the magnitude of the instance of Converter
+        } else if (this.unit.equals("C")) {
+            return this.magnitude * 9 / 5 + 491.67;
+        } else if (this.unit.equals("K")) {
+            return (this.magnitude * 9 / 5);
+        } else if (this.unit.equals("R")) {
             return this.magnitude;
         } else {
             return null;
